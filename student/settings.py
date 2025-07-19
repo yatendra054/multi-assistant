789be163c6a22cc1290605f0dfc1cb8f2c3e2ab7
yatendra054 <yatendrapachori54@gmail.com>
2025-07-19 15:54:14 +0530

    update

--- student/settings.py
+++ student/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-yi$(5+nl%lad%9ex46pq0ojy60nqn49j85rto54bqg7=!p!8dk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
